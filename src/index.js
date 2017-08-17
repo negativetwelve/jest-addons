55eb0e9f2c1f2b28ec2e37f777d7707ec5c94918
Mark Miyashita <negativetwelve@gmail.com>
2017-08-17 02:14:14 -0700

    Export jest-action and add to the setup script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // Addons
+export {default as action} from 'jest-action';
 export {default as context} from 'jest-context';
 export {its, subject} from 'jest-its';
 export {default as set} from 'jest-set';
